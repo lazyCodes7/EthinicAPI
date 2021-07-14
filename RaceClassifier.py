6f82731026ed38dd0d8557db5152860822ae9c46
lazyCodes7 <rishabmudliar@gmail.com>
2021-07-14 11:50:50 +0530

    Add: Pretrained models, and flask app files for the API

---
+++ RaceClassifier.py
@@ -0,0 +1,64 @@
+from __future__ import print_function, division
+import warnings
+warnings.filterwarnings("ignore")
+import os.path
+import pandas as pd
+import torch
+import torch.nn as nn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import dlib
+import os
+import argparse
+import cv2
+class RaceClassifier:
+    def __init__(self,model_path):
+        ## Loading the Resnet34 pretrained model
+        self.model = torchvision.models.resnet34(pretrained=True)
+        # Selecting the device
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # Output has lots of features like the age, race which sum upto 18
+        self.model.fc = nn.Linear(self.model.fc.in_features, 18)
+        #model_fair_7.load_state_dict(torch.load('fair_face_models/fairface_alldata_20191111.pt'))
+
+        # Loading the pretrained model trained on the Fairface dataset
+        self.model.load_state_dict(torch.load(model_path,map_location=torch.device('cpu')))
+        self.model = self.model.to(self.device)
+
+        # Setting eval mode to not compute gradients
+        self.model.eval()
+    
+    # Predict function used to predict the race of the face detected
+    def predict(self,cvImage):
+            trans = transforms.Compose([
+                        transforms.ToPILImage(),
+                        transforms.Resize((224, 224)),
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+            # img pth of face images
+            # list within a list. Each sublist contains scores for all races. Take max for predicted race
+            image = cv2.cvtColor(cvImage, cv2.COLOR_BGR2RGB)        
+            image = trans(image)
+            image = image.view(1, 3, 224, 224)  # reshape image to match model dimensions (1 batch size)
+            image = image.to(self.device)
+
+            # fair
+            outputs = self.model(image)
+            outputs = outputs.cpu().detach().numpy()
+            outputs = np.squeeze(outputs)
+
+            race_outputs = outputs[:7]
+            gender_outputs = outputs[7:9]
+            age_outputs = outputs[9:18]
+
+            race_score = np.exp(race_outputs) / np.sum(np.exp(race_outputs))
+            gender_score = np.exp(gender_outputs) / np.sum(np.exp(gender_outputs))
+            age_score = np.exp(age_outputs) / np.sum(np.exp(age_outputs))
+
+            race_pred = np.argmax(race_score)
+            gender_pred = np.argmax(gender_score)
+            age_pred = np.argmax(age_score)
+
+            return int(race_pred)
---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask, render_template, send_file, make_response, url_for, Response,request,redirect
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+import numpy as np
+import werkzeug
+from RaceClassifier import *
+app = Flask(__name__)
+api = Api(app)
+
+model = RaceClassifier(model_path = "fair_face_models/res34_fair_align_multi_7_20190809.pt")
+
+parser = reqparse.RequestParser()
+parser.add_argument('query')
+
+class PredictRace(Resource):
+    ''' def get(self,image_path):
+        # use parser and find the user's query
+        args = parser.parse_args()
+        user_query = args['query']
+
+        
+        prediction = model.predict(image_name = "detected_faces/race_Latino_face0.jpg")
+
+        # Output either 'Negative' or 'Positive' along with the score
+        if(prediction == 0):
+            pred_text = "White"
+        elif(prediction == 1):
+            pred_text = "Black"
+        elif(prediction == 2):
+            pred_text = "Latino_Hispanic"
+        elif(prediction == 3):
+            pred_text = "East Asian"
+        elif(prediction == 4):
+            pred_text = "Southeast Asian"
+        elif(prediction == 5):
+            pred_text = "Indian"
+        elif(prediction == 6):
+            pred_text = "Middle Eastern"
+            
+ 
+
+        # create JSON object
+        output = {'pred_race': pred_text, 'prediction': prediction} 
+        
+        return output '''
+    def post(self):
+        # read like a stream
+        
+        '''if not request.json or 'image' not in request.json: 
+            abort(400)
+             
+        # get the base64 encoded string
+        im_b64 = request.json['image']
+
+        # convert it into bytes  
+        img_bytes = base64.b64decode(im_b64.encode('utf-8'))
+
+        # convert bytes data to PIL Image object
+        img = Image.open(io.BytesIO(img_bytes))
+
+        # PIL image object to numpy array
+        img_arr = np.asarray(img)      
+        print('img shape', img_arr.shape)'''
+        if (request.method == 'POST'):
+            file = request.files['file'] 
+            if file:
+                ethinicity_pred = []
+                # Loading the haar-cascade face detector
+                face_cascade = cv2.CascadeClassifier('detectors/haarcascade_frontalface_default.xml')
+                # Reading the image sent by the user.
+                img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+                # Detecting the faces in the image
+                faces = face_cascade.detectMultiScale(img, 1.2, 2)
+                # Looping over the faces and getting the ROI
+                for (x,y,w,h) in faces:
+                    roi = img[y:y+h, x:x+w]
+                    # Passing the ROI to our model
+                    prediction = model.predict(roi)
+                    # Getting the ethinicity related to the index passed
+                    ethinicity = PredictRace.getEthinicity(prediction)
+                    ethinicity_pred.append(ethinicity)
+
+                #  Final output consisting of all the races to be returned as a json response
+                output = {'pred_race': ethinicity_pred}
+                return output
+
+    # Static method for predicting the ethinicity based on index passed
+    @staticmethod
+    def getEthinicity(prediction):
+        if(prediction == 0):
+            pred_text = "White"
+        elif(prediction == 1):
+            pred_text = "Black"
+        elif(prediction == 2):
+            pred_text = "Latino_Hispanic"
+        elif(prediction == 3):
+            pred_text = "East Asian"
+        elif(prediction == 4):
+            pred_text = "Southeast Asian"
+        elif(prediction == 5):
+            pred_text = "Indian"
+        elif(prediction == 6):
+            pred_text = "Middle Eastern"
+        return pred_text
+        
+api.add_resource(PredictRace, '/predict')
+if __name__ == '__main__':
+    app.run(debug=True)
